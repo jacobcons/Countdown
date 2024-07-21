2de1ee1fd302c7bdf9a7df05238d12196df9e4b9
Jacob <jacobcons@gmail.com>
2024-07-21 20:31:59 +0000

    ping supabase

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import { verifySessionToken } from './middlewares/auth.middlewares.js';
 import helmet from 'helmet';
 import apiDocs from '../docs/apiDocs.json' with { type: 'json' };
 import swaggerUi from 'swagger-ui-express';
+import { db } from './db/connection.js';
+import { sql } from 'kysely';
 const app = express();
 app.use(helmet());
 app.use(morgan('tiny'));
@@ -27,3 +29,6 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`app listening on port ${port}`);
 });
+// ping supabase on server startup and every 6 days to prevent shutdown
+await sql `SELECT 1`.execute(db);
+setInterval(async () => await sql `SELECT 1`.execute(db), 1000 * 60 * 60 * 24 * 6);
